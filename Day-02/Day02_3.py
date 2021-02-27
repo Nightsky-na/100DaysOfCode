3eaeeed55a152beaf42a6ea06eebd77a1ea73601
Nightsky-na <kontawat.meng@hotmail.com>
2021-02-27 23:25:10 +0700

    Day02

---
+++ Day-02/Day02_3.py
@@ -0,0 +1,20 @@
+#Python Operators
+"""
+Python divides the operators in the following groups:
+- Arithmetic operators
+- Assignment operators
+- Comparison operators
+- Logical operators
+- Identity operators
+- Membership operators
+- Bitwise operators
+"""
+#Python Assignment Operators
+#https://www.geeksforgeeks.org/python-operators/
+# //=	x //= 3	x = x // 3
+x = 5
+print("quotient = " + str(x//3)) #Divide and keep quotient
+# %=	x %= 3	x = x % 3
+print(x % 3) #Remnant = 2
+print(x ** 3) #power
+print(x & 3)
